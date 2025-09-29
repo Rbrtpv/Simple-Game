094bfb4e9908d85713c9cfa09ac6bb29d6a84c7f
Ro7 <123015624+Rbrtpv@users.noreply.github.com>
2025-09-29 00:03:55 -0400

    feat(Machines): Add constructor parameters and draw method

--- src/entities/enemies/Machines.java
+++ src/entities/enemies/Machines.java
@@ -1,9 +1,15 @@
 package entities.enemies;
 
-import entities.characters.GameCharacter;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Machines extends GameCharacter {
+public class Machines {
 
-    public Machines() {
+    public Machines(int x, int y, int w, int h, int speed) {
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.orange);
+        g.drawOval(300, 500, 50, 50);
     }
 }

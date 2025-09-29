461abd7aaf9aa0b77579353c2893229e7790ac79
Ro7 <123015624+Rbrtpv@users.noreply.github.com>
2025-09-29 00:00:49 -0400

    feat(Slash): update to support area-of-effect attacks
    This commit replaces the basic Slash character properties with a list of area-of-effect attacks.
    Modified the update() method to handle these attacks instead of printing a message.
    Added an attack() method to create and store new area-of-effect attacks.
    Added necessary imports for AreaOfEffect and Java collections.
    
    The changes implement a more dynamic attack system for the Slash character.

--- src/entities/characters/Slash.java
+++ src/entities/characters/Slash.java
@@ -1,20 +1,33 @@
 package entities.characters;
 
+import entities.dmg.AreaOfEffect;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Slash extends GameCharacter {
 
-    private int x, y, w, h, speed;
+    private List<AreaOfEffect> attacks;
 
     public Slash(int x, int y, int w, int h, int speed) {
-        super(x, y, w, h, speed);
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.speed = speed;
+        super(x, y, w, h, 8, Color.WHITE, 100, 20, 10, "Slash");
+        this.attacks = new ArrayList<>();
     }
 
     @Override
     public void update() {
-        System.out.println("update slash");
+        for (AreaOfEffect attack : attacks) {
+            attack.update();
+        }
+        attacks.removeIf(AreaOfEffect::isFinished);
+    }
+
+    @Override
+    public void attack(int x, int y) {
+        attacks.add(new AreaOfEffect(getX(), getY(), 150, 150, 10, Color.RED));
+    }
+
+    public List<AreaOfEffect> getAttacks() {
+        return attacks;
     }
 }

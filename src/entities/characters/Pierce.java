42264df872619d79fb2fbf5b3f7e8d179f1eb430
Ro7 <123015624+Rbrtpv@users.noreply.github.com>
2025-09-29 00:00:09 -0400

    feat(Pierce): add projectile firing capability *   Modified constructor to call super with extended parameters including color and health. *   Added `List<Projectile>` field for tracking projectiles. *   Updated `update()` method to iterate and update all active projectiles. *   Implemented `attack()` method to instantiate and add a new projectile towards target coordinates.

--- src/entities/characters/Pierce.java
+++ src/entities/characters/Pierce.java
@@ -1,20 +1,34 @@
 package entities.characters;
 
+import entities.dmg.Projectile;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pierce extends GameCharacter {
 
-    private int x, y, w, h, speed;
+    private List<Projectile> projectiles;
 
     public Pierce(int x, int y, int w, int h, int speed) {
-        super(x, y, w, h, speed);
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.speed = speed;
+        super(x, y, w, h, 7, Color.BLUE, 100, 20, 150, "Pierce");
+        this.projectiles = new ArrayList<>();
     }
 
     @Override
     public void update() {
-        System.out.println("update pierce");
+        for (Projectile projectile : projectiles) {
+            projectile.update();
+        }
+    }
+
+    @Override
+    public void attack(int x, int y) {
+        projectiles.add(new Projectile(getX() + (getW() / 2 - 5), getY() + getH() / 2 - 5, 10, 10,
+                25, x, y, Color.YELLOW));
+    }
+
+    // Getters & Setters
+    public List<Projectile> getProjectiles() {
+        return projectiles;
     }
 }

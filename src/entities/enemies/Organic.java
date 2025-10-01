4632df34e74a64de0063001851e9045a1d6bc12f
Ro7 <123015624+Rbrtpv@users.noreply.github.com>
2025-09-30 22:28:45 -0400

    feat(organic): implement drawing and update behavior by extending Enemy class
    Refactored Organic to extend Enemy for proper drawing and update support. Added drawing methods and override update to enable chasing behavior. This change ensures consistent enemy handling while adding visual representation and dynamic movement.

--- src/entities/enemies/Organic.java
+++ src/entities/enemies/Organic.java
@@ -1,9 +1,38 @@
 package entities.enemies;
 
-import entities.characters.GameCharacter;
+import java.awt.Color;
+import java.awt.Graphics;
+import user.Player;
 
-public class Organic extends GameCharacter {
+public class Organic extends Enemy {
 
     public Organic() {
+        super(410, 30, 100, 100, 2, 1000,
+                1000, 60, 400,
+                200.0f, 0.3f);
+    }
+
+    @Override
+    public void update() {
+        chase();
+    }
+
+    public void draw(Graphics g) {
+        drawEnemy(g);
+        healthBar(g);
+    }
+
+    @Override
+    public void drawEnemy(Graphics g) {
+        g.setColor(Color.red);
+        g.fillRect(x, y, w, h);
+        g.drawString("O ", x, y - 10);
+    }
+
+    @Override
+    public void attack(Player target) {
+        if (checkIfTargetInRange(target)) {
+            target.getCurrentCharacter().takeDamage(attackDmg);
+        }
     }
 }
